9699a87e8e7c229a354830878477c3899ff2d296
GOZMAN001 <yosefmichayahnco@gmail.com>
2024-02-12 22:25:59 +0100

    j.son

--- lib/dart_basic.dart
+++ lib/dart_basic.dart
@@ -5,4 +5,17 @@ void main() {
   print(name);
   var x = 10;
   var y = 50;
+  var value = x / 2;
+  print(value);
+  x = 10;
+  y = 50;
+  if (x <= 5) ;
+  print(true);
+  print(perimeterofarectangle(30, 10));
+}
+
+num perimeterofarectangle(l, w) {
+  num l = 30;
+  num w = 10;
+  return 2 * (l + w);
 }
